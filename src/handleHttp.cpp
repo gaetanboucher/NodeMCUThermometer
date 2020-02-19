a60ba689f73f4e5ea78cdb9353a7c959cd42dbb0
Gaetan Boucher <gaetan.boucher@gmail.com>
2020-02-18 22:34:41 -0500

    Change home page font style and clean wifi page

--- src/handleHttp.cpp
+++ src/handleHttp.cpp
@@ -27,6 +27,8 @@ extern float getProbeTemp(int prodeId);
 std::string getMenu();
 std::string getStyleSheet();
 std::string getPage();
+std::string getProbes();
+std::string getWifi();
 
 /** Handle root or redirect to captive portal */
 void handleRoot() {
@@ -34,23 +36,28 @@ void handleRoot() {
     return;
   }
   std::string page = getPage();
+  std::string probes = getProbes();
+  
+  page.replace(page.find("*Center*"),strlen("*Center*"), probes);
+
+  server.send(200, "text/html", page.c_str());
+}
+
+std::string getProbes() {
   std::string probes;
-  probes += "      <div class='Center-Top'>";
-  probes += "        <p>";
+  probes  = "      <div class='Center-Top'>";
+  probes += "        <p class='probe'>";
   probes +=            String(getProbeTemp(0)).c_str();
   probes += "          &deg;C";
   probes += "        </p>";
   probes += "      </div>";
   probes += "      <div class='Center-Bottom'>";
-  probes += "        <p>";
+  probes += "        <p class='probe'>";
   probes +=            String(getProbeTemp(1)).c_str();
   probes += "          &deg;C";
   probes += "        </p>";
   probes += "      </div>";
-  
-  page.replace(page.find("*Center*"),strlen("*Center*"), probes);
-
-  server.send(200, "text/html", page.c_str());
+  return probes;  
 }
 
 std::string getPage() {
@@ -80,13 +87,13 @@ std::string getPage() {
   Page += "  </div>";
   Page += "  <div class='Foot'>";
   Page += "    <div class='Battery'>";
-  Page +=        "<p>";
+  Page +=        "<p class='foot'>";
   float batteryLevel = ESP.getVcc()*100.0/65353.0;
   Page +=           String(batteryLevel).c_str();
   Page +=        "</p>";
   Page += "    </div>";
   Page += "    <div class='Network'>";
-  Page += "      <p>";
+  Page += "      <p class='foot'>";
   Page += WiFi.SSID().c_str();
   Page += "      </p>";
   Page += "    </div>";
@@ -171,97 +178,74 @@ boolean captivePortal() {
 
 /** Wifi config page handler */
 void handleWifi() {
-  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-  server.sendHeader("Pragma", "no-cache");
-  server.sendHeader("Expires", "-1");
+  std::string page = getPage();
 
+  page.replace(page.find("*Center*"),strlen("*Center*"), getWifi());
+
+  server.send(200, "text/html", page.c_str());
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+
+std::string getWifi() {
   std::string Page;
-  Page += "<html>";
-  Page += "<head>";
-  Page += getStyleSheet();
-  Page += "</head>";
-  Page += "<div class='grid-container'>";
-  Page += "  <div class='Head'>";
-  Page += "    <div class='Menu' id='h_nav_bar'>";
-  Page += getMenu();
-  Page += "    </div>";
-  Page += "    <div class='Title'>";
-  Page += "    <h1><b>Wifi Thermometer</b></h1>";
-  Page += "    </div>";
-  Page += "  </div>";
-  Page += "  <div class='Body'>";
-  Page += "    <div class='Center'>";
+  Page += "<table><tr><th align='left' colspan=2>";
   if (server.client().localIP() == apIP) {
     Page += "<p>You are connected through the soft AP: ";
+    Page += "</th><td>";
     Page += softAP_ssid;
     Page += "</p>";
+    Page += "</td>";
   } else {
     Page += "<p>You are connected through the wifi network: ";
+    Page += "</th><td>";
     Page += WiFi.SSID().c_str();
     Page += "</p>";
+    Page += "</td>";
   }
-  Page += "<table><tr><th align='left'>SoftAP config</th></tr>";
-  Page +=     "<tr><td>SSID ";
+  Page +=  "</tr>";
+  Page += "<tr><th align='left' colspan=2>SoftAP config</th></tr>";
+  Page +=     "<tr><td>SSID</td><td>";
   Page += WiFi.softAPSSID().c_str();
   Page +=   "</td></tr>";
-  Page +=   "<tr><td>IP ";
+  Page +=   "<tr><td>IP</td><td>";
   Page += toStringIp(WiFi.softAPIP()).c_str();
   Page +=  "</td></tr>";
-  Page +=  "</table>";
-  Page +=  "<br />";
-  Page +=  "<table><tr><th align='left'>WLAN config</th></tr>";
-  Page +=  "<tr><td>SSID ";
+  Page +=  "<tr><th align='left' colspan=2>WLAN config</th></tr>";
+  Page +=  "<tr><td>SSID</td><td>";
   Page +=  WiFi.SSID().c_str();
   Page +=  "</td></tr>";
-  Page +=  "<tr><td>IP ";
+  Page +=  "<tr><td>IP</td><td>";
   Page +=  toStringIp(WiFi.localIP()).c_str();
   Page +=  "</td></tr>";
-  Page +=  "</table>";
-  Page +=  "<br />";
-  Page +=  "<table><tr><th align='left'>WLAN list (refresh if any missing)</th></tr>";
+  Page +=  "";
+  Page +=  "<tr><td align='left'>WLAN list</td>";
   Serial.println("scan start");
   int n = WiFi.scanNetworks();
   Serial.println("scan done");
+  Page += "<td>";
   if (n > 0) {
     for (int i = 0; i < n; i++) {
-      Page += "<tr><td>SSID ";
+      Page += ((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "&#128274;"); 
       Page += WiFi.SSID(i).c_str();
-      Page += ((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : " *"); 
       Page += " ("; 
       Page += String(WiFi.RSSI(i)).c_str();
-      Page += ")</td></tr>";
-      Page += "<tr><td>SSID ";
-      Page += WiFi.SSID(i).c_str();
-      Page += ((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : " *");
-      Page += " (";
-      Page += String(WiFi.RSSI(i)).c_str();
-      Page += ")</td></tr>";
+      Page += ")<br/>";
     }
   } else {
-    Page += "<tr><td'>No WLAN found</td></tr>";
+    Page += "No WLAN found";
   }
-  Page += "</table>";
-  Page +=   "\r\n<br /><form method='POST' action='wifisave'><h4>Connect to network:</h4>";
+  Page += "</td>";
+  Page += "</tr>";
+  Page += "<tr><td>";
+  Page +=   "<form method='POST' action='wifisave'><h4>Connect to network:</h4>";
+  Page += "</td>";
+  Page += "<td>";
   Page +=   "<input type='text' placeholder='network' name='n'/>";
-  Page +=   "<br /><input type='password' placeholder='password' name='p'/>";
-  Page +=   "<br /><input type='submit' value='Connect/Disconnect'/></form>";
-  Page += "<div>";
-  Page += "    <div class='Left'></div>";
-  Page += "    <div class='Right'></div>";
-  Page += "  </div>";
-  Page += "  <div class='Foot'>";
-  Page += "    <div class='Battery'><p>";
-  float batteryLevel = ESP.getVcc()*100.0/65353.0;
-  Page += "      <p>";
-  Page += batteryLevel;
-  Page += "</p>";
-  Page += "    </div>";
-  Page += "    <div class='Network'><p>NetworkID</p></div>";
-  Page += "    <div class='Status'></div>";
-  Page += "  </div>";
-  Page += "</div>";
-  server.send(200, "text/html", Page.c_str());
-  server.client().stop(); // Stop is needed because we sent no content length
+  Page +=   "<input type='password' placeholder='password' name='p'/>";
+  Page +=   "<input type='submit' value='Connect/Disconnect'/></form>";
+  Page += "</td></tr>";
+  Page += "</table>";
+  return Page;
 }
 
 void handleSetup() {
